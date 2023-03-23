2444f316793b1e038878d190702c9e785ef0e958
Elli Beres <elliberes@proton.me>
2023-03-23 14:42:10 -0500

    tst4

--- test.py
+++
@@ -1,7 +0,0 @@
-import pyflowbat as pfb
-
-
-my_wrkspc = pfb.Workspace()
-
-print(my_wrkspc.lims)
-
--- test.tst.py
+++
@@ -1,6 +0,0 @@
-import pyflowbat as pfb
-
-my_wrkspc = pfb.Workspace()
-
-print(my_wrkspc.lims)
-
