3968e8bf32552404028faa9b653135b66a87d761
Elli Beres <elliberes@proton.me>
2023-07-03 01:33:14 -0500

    Update docs

--- pyflowbat/docs/conf.py
+++ pyflowbat/docs/conf.py
@@ -21,7 +21,7 @@ release = '0.0.1'
 extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '*/setup.py']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '*/setup*']
 
 autodoc_mock_imports = ["rpy2"]
 
